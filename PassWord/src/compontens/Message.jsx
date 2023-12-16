463ab276d6f83e625469a962d0fd21d56e9083da
prabhavathi2501 <prabhashok2501@gmail.com>
2023-12-16 19:04:37 +0530

    message link update

--- PassWord/src/compontens/Message.jsx
+++ PassWord/src/compontens/Message.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Card from "react-bootstrap/Card"
+import CardBody from 'react-bootstrap/esm/CardBody'
+import { useNavigate } from 'react-router-dom';
+
 
 function Message() {
+  let navigate =useNavigate("")
   return<>
   <div className='container-fluid'>
+    <Card>
+      <Card.Body>
   <h1 className='email-link'>link  sent your email pls check your mail</h1>
+  <p className='or'>or</p>
+
+  <a herf="#"  className="message" onClick={()=>navigate("/login")} ><h4>signin</h4></a>
+  </Card.Body>
+  </Card>
   </div>
   </>
 }
